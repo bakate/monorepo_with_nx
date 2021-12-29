d969f6de5a4f20a59ff1200e7af7cd706e93a586
Bakate <b.ba@startupflow.io>
2021-12-30 00:05:28 +0100

    feat(bakate-blog): add a mainPage component to wrap the next.js parent component _app

--- apps/bakate-blog/components/page/mainPage.tsx
+++ apps/bakate-blog/components/page/mainPage.tsx
@@ -1,35 +1,43 @@
-/* eslint-disable-next-line */
-;
-import { ethicalClothesTheme,ChakraHeader, ChakraContainer } from "@bakate-organization/chakra-ui";
+import {
+  ethicalClothesTheme,
+  ChakraHeader,
+  ChakraContainer,
+  Meta,
+} from '@bakate-organization/chakra-ui';
 import { ChakraProvider } from '@chakra-ui/react';
 import Router from 'next/router';
-import NProgress from 'nprogress'
+import NProgress from 'nprogress';
 import React from 'react';
-import Meta from '../meta/meta';
 
 export interface PageProps {
   children?: React.ReactElement;
 }
 
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on('routeChangeComplete', () => NProgress.done());
 
-Router.events.on('routeChangeStart',()=> NProgress.start())
-Router.events.on('routeChangeError', ()=> NProgress.done())
-Router.events.on('routeChangeComplete',()=> NProgress.done())
-
-export function Page({children}: PageProps) {
-  const links = [{
-    href: '/', label:'home'
-  }, {href:'/articles', label:'articles'}, {href:'/about', label:'about'}]
+export function Page({ children }: PageProps) {
+  const links = [
+    {
+      href: '/',
+      label: 'home',
+    },
+    { href: '/articles', label: 'articles' },
+    { href: '/about', label: 'about' },
+  ];
 
   return (
     <ChakraProvider theme={ethicalClothesTheme}>
-      <Meta/>
-      <ChakraHeader linksProp={links} title={"Tech Blog"} />
-      <ChakraContainer>
-        {children}
-      </ChakraContainer>
-
+      <Meta
+        title={'bakate-blog'}
+        description={
+          'a blog to share my knowledge about JavaScript, frontend and backend development'
+        }
+        url={'/nprogress.css'}
+      />
+      <ChakraHeader linksProp={links} title={'Tech Blog'} />
+      <ChakraContainer>{children}</ChakraContainer>
     </ChakraProvider>
   );
 }
-
