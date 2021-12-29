c1e35b59483757acb5de64d8b73c5a389751c111
Bakate <b.ba@startupflow.io>
2021-12-29 14:17:51 +0100

    feat(chakra-ui): create chakra-ui lib to share different ui components

---
+++ libs/chakra-ui/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'chakra-ui',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/chakra-ui',
+};
